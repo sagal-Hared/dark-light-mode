5f6a0b55fca33081d76f2fb1b814744568cb9f25
Sagal-Hared <sagalhared2@gmail.com>
2024-06-13 16:05:03 +0300

    my first light mode

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const dark=document.getElementById("dark-light-mode");
+const body=document.getElementById("body")
+dark.addEventListener("click", ()=>{
+    body.classList.toggle("dark-mode");
+    
+})
